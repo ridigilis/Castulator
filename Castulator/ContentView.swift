f30949c9ed8a0bcffc89cc1be696864682898b9a
rdgronii <rgroner2@gmail.com>
2023-11-28 14:45:24 -0600

    add roll func and result state on button press

--- Castulator/ContentView.swift
+++ Castulator/ContentView.swift
@@ -8,35 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var result: UInt?
+    
+    enum Dice {
+        case d4, d6, d8, d10, d12, d20, d100
+    }
+    
+    func roll(_ die: Dice) -> UInt {
+        switch die {
+        case .d4: return UInt.random(in: 1...4)
+        case .d6: return UInt.random(in: 1...6)
+        case .d8: return UInt.random(in: 1...8)
+        case .d10: return UInt.random(in: 0...9)
+        case .d12: return UInt.random(in: 1...12)
+        case .d20: return UInt.random(in: 1...20)
+        case .d100: return UInt.random(in: 0...99)
+        }
+    }
     var body: some View {
         VStack {
             Spacer()
-            Text("Roll Result goes here!")
+            
+            result != nil
+                ? Text(String(result!))
+                : Text("Click any die below!")
+            
             Spacer()
             
             Grid {
                 GridRow {
-                    Button {} label: {
+                    Button { result = roll(.d4) } label: {
                         Image("d4").resizable().scaledToFit()
                     }
-                    Button {} label: {
+                    Button { result = roll(.d6) } label: {
                         Image("d6").resizable().scaledToFit()
                     }
-                    Button {} label: {
+                    Button { result = roll(.d8) } label: {
                         Image("d8").resizable().scaledToFit()
                     }
-                    Button {} label: {
+                    Button { result = roll(.d10) } label: {
                         Image("d10").resizable().scaledToFit()
                     }
                 }
                 GridRow {
-                    Button {} label: {
+                    Button { result = roll(.d12) } label: {
                         Image("d12").resizable().scaledToFit()
                     }
-                    Button {} label: {
+                    Button { result = roll(.d20) } label: {
                         Image("d20").resizable().scaledToFit()
                     }
-                    Button {} label: {
+                    Button { result = roll(.d100) } label: {
                         Image("d100").resizable().scaledToFit()
                     }
                 }
