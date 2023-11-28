673bc69460645625a3b57bbd7927420d9cce5b29
rdgronii <rgroner2@gmail.com>
2023-11-28 14:16:37 -0600

    add buttons with dice images and placeholder text

--- Castulator/ContentView.swift
+++ Castulator/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Roll Result goes here!")
+            Spacer()
+            
+            Grid {
+                GridRow {
+                    Button {} label: {
+                        Image("d4").resizable().scaledToFit()
+                    }
+                    Button {} label: {
+                        Image("d6").resizable().scaledToFit()
+                    }
+                    Button {} label: {
+                        Image("d8").resizable().scaledToFit()
+                    }
+                    Button {} label: {
+                        Image("d10").resizable().scaledToFit()
+                    }
+                }
+                GridRow {
+                    Button {} label: {
+                        Image("d12").resizable().scaledToFit()
+                    }
+                    Button {} label: {
+                        Image("d20").resizable().scaledToFit()
+                    }
+                    Button {} label: {
+                        Image("d100").resizable().scaledToFit()
+                    }
+                }
+            }
         }
         .padding()
+        .background(Image("parchment"))
     }
 }
 
