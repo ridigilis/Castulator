8389392e274be2f3c445cc8c33ab8e7d21226ffd
rdgronii <rgroner2@gmail.com>
2023-11-28 12:00:04 -0600

    Initial Commit

---
+++ Castulator/CastulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastulatorApp.swift
+//  Castulator
+//
+//  Created by Ricky David Groner II on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CastulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
